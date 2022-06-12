9b5b6f4c7bf705a6c27129d2b29f2a4dc3c1161d
tharindu13 <93427493+tharindu13@users.noreply.github.com>
2022-06-13 00:38:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
 
-global.owner = ['6281351047727'] // Put your number here
+global.owner = ['94753240198'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
 global.APIs = { // API Prefix
@@ -29,8 +29,8 @@ global.APIKeys = { // APIKey Here
 // Sticker WM
 global.stiker_wait = 'Stiker sedang dibuat'
 global.packname = 'ShiraoriBOT Multi device'
-global.author = 'Ilmanhdyt'
-global.namabot = 'ShiraoriBOT'
+global.author = 'THARINDU'
+global.namabot = 'LAMBA'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.wm = '© ShiraoriBOT-Md • Ilmanhdyt'
 global.media = 'https://telegra.ph/file/5d45a3d1e12726d98c5e4.jpg'
@@ -39,11 +39,11 @@ global.wait = '_*tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
 
 global.dtu = 'ɪɴꜱᴛᴀɢʀᴀᴍ'
-global.urlnya = "https://www.instagram.com/ilmanhdyt_"
+global.urlnya = "https://www.instagram.com/"
 
 //============= callButtons =============//
 global.dtc = 'ᴄᴀʟʟ ᴏᴡɴᴇʀ'
-global.phn = '+62 813-5104-7727'
+global.phn = '+94-753240198'
 
 //============= Games ================//
 global.benar = '_*Benar✅*_'
